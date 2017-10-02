a286b1ffb4f08c681b51f3b249d92de7ccbb941d
Cesar Porcayo <cesarporcayo@gatech.edu>
2017-10-02 14:44:53 -0400

    Finished functionality of milestone

--- M4/app/src/main/java/edu/gatech/m4/Registration.java
+++ M4/app/src/main/java/edu/gatech/m4/Registration.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
 import java.util.HashMap;
 
@@ -18,8 +19,12 @@ public class Registration extends AppCompatActivity {
     private EditText email;
     private EditText password;
     //Buttons
-    private Button cancel;
-    private Button register;
+    private Button Cancel;
+    private Button Register;
+    //Radio Buttons
+    private RadioButton Admin;
+    private RadioButton User;
+    //Hash Map to store user registrations
     public static HashMap<String,String> user_data;
     private EditText error;
 
@@ -27,22 +32,31 @@ public class Registration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         //Map UI to variables
         name = (EditText)findViewById(R.id.name_registration);
         email = (EditText)findViewById(R.id.email_registration);
         password = (EditText)findViewById(R.id.password_registration);
-        register = (Button) findViewById(R.id.registration_button);
-        cancel = (Button) findViewById(R.id.cancel_registration_button);
+        Register = (Button) findViewById(R.id.registration_button);
+        Cancel = (Button) findViewById(R.id.cancel_registration_button);
+        Admin = (RadioButton) findViewById(R.id.radio_admin);
+        User = (RadioButton) findViewById(R.id.radio_user);
         error = (EditText) findViewById(R.id.error_message);
         //Create hashmap
         user_data = new HashMap<String, String>();
-        register.setOnClickListener(new View.OnClickListener() {
+        Register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //make sure user not already in hashmap
-                if (user_data.containsKey(email.getText())) {
+                //checks to see if a valid email address has been used to register
+                if (name.getText().toString().isEmpty()) {
+                    error.setText("'Name' field cannot be empty");
+                } else if (!email.getText().toString().contains("@")) {
+                    error.setText("Not a valid email address");
+                } else if (user_data.containsKey(email.getText().toString())) { //make sure user not already in hashmap
                     error.setText("You're already registered fool!");
+                } else if (password.getText().toString().isEmpty()) {
+                    error.setText("'Password' field cannot be empty");
                 } else {
                     //add user to hashmap
                     user_data.put(email.getText().toString(), password.getText().toString());
@@ -51,11 +65,34 @@ public class Registration extends AppCompatActivity {
                 }
             }
         });
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        Cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Registration.this, WelcomeActivity.class );
+                startActivity(intent);
+            }
+        });
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_registration);
         //setup the registration form.
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_admin:
+                if (checked)
+                    // Admin
+                    break;
+            case R.id.radio_user:
+                if (checked)
+                    // User
+                    break;
+        }
     }
 
     public static HashMap<String, String> getUser_data() {
