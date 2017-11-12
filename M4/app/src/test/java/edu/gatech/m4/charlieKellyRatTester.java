08e62eb28d05e722fae404e43221c072c72efd66
Murphy <Murphy@lawn-128-61-52-146.lawn.gatech.edu>
2017-11-12 14:40:47 -0500

    Added a test file and set updependencies.

---
+++ M4/app/src/test/java/edu/gatech/m4/charlieKellyRatTester.java
@@ -0,0 +1,19 @@
+package edu.gatech.m4;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.*;
+
+
+/**
+ * Created by Murphy on 11/12/17.
+ */
+
+public class charlieKellyRatTester {
+
+    @Test
+    public void mapTester() throws Exception {
+        int x = 2 + 2;
+        assert (x == 2);
+    }
+}
